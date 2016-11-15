56ff45f0ce11cc6344adf4e42ed65f1335e7e63d
Matthias Eichstedt <matthias.eichstedt@mesosphere.io>
2016-11-15 14:39:33 +0100

    comment

--- Foxy.scala
+++ Foxy.scala
@@ -2,5 +2,6 @@
   * Represents a foxy and it's associated foos.
   * @param id Id of the foxy
   * @param values all associated foos by name
+  * Use this with caution!
   */
 case class Foxy(id: String, foosByName: Map[String, Foo])
